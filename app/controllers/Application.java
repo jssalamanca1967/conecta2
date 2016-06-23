2beb333ec7b94fcb8b5877b1f045b79a6327caf4
Juan José <jj.diaz1067@uniandes.edu.co>
2016-06-23 18:36:15 -0500

    Encrypt password

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -6,6 +6,9 @@ import play.data.Form;
 import play.mvc.*;
 import play.data.*;
 import views.html.*;
+import models.Usuario;
+import java.util.List;
+import play.libs.Crypto;
 
 public class Application extends Controller {
 
@@ -24,40 +27,37 @@ public class Application extends Controller {
         DynamicForm form = Form.form().bindFromRequest();
         String username = form.get("username");
         String password = form.get("password");
+        Logger.info("Capto el mensaje "+ username +  " "+password);
+        List<Usuario> lista = Usuario.find.all();
+        for (Usuario u : lista) {
+            Logger.info(u.email);
+        }
+        Usuario user = null;
+        boolean encontro = false;
+        for (Usuario i: lista){
+            if (i.email.equals(username)){
+                user = i;
+                Logger.info("Encontro a un usuario");
+                encontro = true;
+                break;
+            }
+        }
+        if (!encontro){
+            return badRequest("No se encontró un usuario con este correo");
+        }
+        if (!checkPassword(password, user.contrasenia)){
+            return badRequest("La contraseña es incorrecta");
+        }
+        Logger.info("Paso la prueba de contraseña");
+        session().clear();
+        session("username", username);
         return ok(login.render(form));
     }
 
-    public Result login() {
-        return ok(
-                login.render(form(Application.Login.class))
-        );
-    }
-
-    public Result authenticate() {
-        Form<Login> loginForm = form(Login.class).bindFromRequest();
-        if (loginForm.hasErrors()) {
-            return badRequest(login.render(loginForm));
-        } else {
-            session().clear();
-            session("email", loginForm.get().email);
-            return redirect(
-                    routes.Application.index()
-            );
-        }
-    }
-
-    public String validate() {
-        if (User.authenticate(email, password) == null) {
-            return "Invalid user or password";
-        }
-        return null;
-    }
-
-    public class Login {
-
-        public String email;
-        public String password;
-
+    public boolean checkPassword(String webPassword, String password){
+        String encoded = play.Play.application().injector().instanceOf(Crypto.class).sign(webPassword);
+        Logger.info("La contra codificada es "+ encoded);
+        return encoded.equals(password);
     }
 }
 
