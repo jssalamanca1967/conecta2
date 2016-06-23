3af9d79e6b5ca8f1a730eeea78864cade0288c99
John <js.salamanca1967@uniandes.edu.co>
2016-06-23 18:33:46 -0500

    Usuario - CodigoQR Done

--- app/controllers/ClienteController.java
+++ app/controllers/ClienteController.java
@@ -1,7 +1,41 @@
 package controllers;
 
+import play.mvc.*;
+
 /**
  * Created by John on 17/06/2016.
  */
-public class ClienteController {
+public class ClienteController extends Controller {
+
+    public Result crear() {
+        return Results.TODO;
+    }
+
+    public Result guardarNuevo() {
+        return Results.TODO;
+    }
+
+    public Result guardarEditado(long id) {
+        return Results.TODO;
+    }
+
+    public Result guardarCodigo(long id) {
+        return Results.TODO;
+    }
+
+    public Result redimirCodigo(long id) {
+        return Results.TODO;
+    }
+
+    public Result editar(long id) {
+        return Results.TODO;
+    }
+
+    public Result mostrar(long id) {
+        return Results.TODO;
+    }
+
+    public Result clientes() {
+        return Results.TODO;
+    }
 }
