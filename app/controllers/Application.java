1639df187d4017b924adef265b7b62c07e460d9e
Juan José <jj.diaz1067@uniandes.edu.co>
2016-06-24 01:38:28 -0500

    Completion of login form for roles

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -35,7 +35,6 @@ public class Application extends Controller {
     }
 
     public Result loginSubmit(){
-
         DynamicForm form = Form.form().bindFromRequest();
         String username = form.get("username");
         String password = form.get("password");
@@ -59,19 +58,65 @@ public class Application extends Controller {
         }
         Logger.info("Paso la prueba de contraseña");
         session().clear();
-        session("auth", "admin");
+        session("auth", "user");
         return ok(index.render());
     }
 
     public Result loginSubmitCliente(){
+        DynamicForm form = Form.form().bindFromRequest();
+        String username = form.get("username");
+        String password = form.get("password");
+        Logger.info("Capto el mensaje "+ username +  " "+password);
+        List<Cliente> lista = Cliente.find.all();
+        Cliente user = null;
+        boolean encontro = false;
+        for (Cliente i: lista){
+            if (i.nit.equals(username)){
+                user = i;
+                Logger.info("Encontro a un Cliente");
+                encontro = true;
+                break;
+            }
+        }
+        if (!encontro){
+            return badRequest("No se encontró un Cliente con este nit");
+        }
+        if (!checkPassword(password, user.contrasenia)){
+            return badRequest("La contraseña es incorrecta");
+        }
+        Logger.info("Paso la prueba de contraseña");
+        session().clear();
+        session("auth", "client");
         return ok(index.render());
-
     }
 
 
     public Result loginSubmitPersonal(){
+        DynamicForm form = Form.form().bindFromRequest();
+        String username = form.get("username");
+        String password = form.get("password");
+        Logger.info("Capto el mensaje "+ username +  " "+password);
+        List<Personal> lista = Personal.find.all();
+        Personal user = null;
+        boolean encontro = false;
+        for (Personal i: lista){
+            if (i.nombreUsuario.equals(username)){
+                user = i;
+                Logger.info("Encontro a un empleado");
+                encontro = true;
+                break;
+            }
+        }
+        if (!encontro){
+            return badRequest("No se encontró un empleado con este nombre de usuario");
+        }
+        if (!checkPassword(password, user.contrasenia)){
+            return badRequest("La contraseña es incorrecta");
+        }
+        Logger.info("Paso la prueba de contraseña");
+        session().clear();
+        session("auth", "personal");
         return ok(index.render());
-
     }
 
     public boolean checkPassword(String webPassword, String password){
